fac5a81f2f96ad8c31a85bc9db6fc44551c34e1e
hateshinies <bigdick@gmai.com>
2016-07-22 13:01:43 +0300

    test commit

--- src/main/java/base/repositories/UploadRepository.java
+++ src/main/java/base/repositories/UploadRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+
 public interface UploadRepository extends CrudRepository<Upload, Integer> {
     Upload findOne(Integer id);
     List<Upload> findAll();
